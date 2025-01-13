9c9b2bc54407d116b3b9282e87897f14ba45d30b
ranju999 <ranjuvishwakarma.50@gmail.com>
2025-01-13 07:48:56 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
 import re
+from pyrogram.types import *
+from typing import Any
+from pyrogram import enums
 
 
 class temp(object):
@@ -15,3 +18,25 @@ def get_size(size):
         i += 1
         size /= 1024.0
     return "%.2f %s" % (size, units[i])
+
+def get_file_id(message: "Message") -> Any:
+    media_types = (
+        "audio",
+        "document",
+        "photo",
+        "sticker",
+        "animation",
+        "video",
+        "voice",
+        "video_note",
+    )    
+    if message.media:
+        for attr in media_types:
+            media = getattr(message, attr, None)
+            if media:
+                setattr(media, "message_type", attr)
+                return media
+
+def get_hash(media_msg: Message) -> str:
+    media = get_file_id(media_msg)
+    return getattr(media, "file_unique_id", "")[:6]
